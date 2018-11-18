c563c9f50f908935732d07e4d80adcbeef51db2a
Zlash65 <zarrar65@gmail.com>
2018-11-18 16:40:07 +0530

    model created to store data in db

--- bank/models.py
+++ bank/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+	''' Bank model '''
+	ifsc = models.CharField(max_length=20, primary_key=True)
+	bank_id = models.IntegerField(default=0)
+	branch = models.CharField(max_length=30)
+	address = models.CharField(max_length=150)
+	city = models.CharField(max_length=30)
+	district = models.CharField(max_length=30)
+	state = models.CharField(max_length=30)
+	bank_name = models.CharField(max_length=50)
